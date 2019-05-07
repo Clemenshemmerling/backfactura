397d9c133ea57a49584065b48c660e955638a970
Clemens Hemmerling <clemens@grupomacro.com>
2019-05-07 14:10:13 -0600

    cambio de tipo de pago

--- app/app.js
+++ app/app.js
@@ -12,7 +12,15 @@ const fs = require("fs"),
   decode = require('unescape'),
   xadesjs = require("xadesjs"),
   qs = require('qs');
-  
+ 
+ const banco =  
+ `<?xml version="1.0" encoding="utf-8"?>
+ <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+   <soap:Body>
+     <TipoCambioDia xmlns="http://www.banguat.gob.gt/variables/ws/" />
+   </soap:Body>
+ </soap:Envelope>`;
+
 const postedData =
   `<?xml version='1.0' encoding='UTF-8'?>
   <SolicitaTokenRequest>
@@ -163,8 +171,19 @@ io.sockets.on('connect', socket => {
     });
   });
   socket.on('solicitud', (res) => {
-    axios.get('https://free.currencyconverterapi.com/api/v6/convert?q=USD_GTQ&compact=ultra&apiKey=157e0765190fd121de41').then(res => {
-      socket.emit('cambio', res.data);
+    axios.post('http://www.banguat.gob.gt/variables/ws/TipoCambio.asmx', banco, {
+      headers: {
+        'content-type': 'text/xml'
+      }
+    })
+    .then(res => {
+      let cambio = convert.xml2js(res.data, {compact: true, spaces: 2});
+      let tipo = cambio['soap:Envelope']['soap:Body'].TipoCambioDiaResponse.TipoCambioDiaResult.CambioDolar.VarDolar. referencia._text;
+      socket.emit('cambio', tipo);
+      console.log(tipo);
+    })
+    .catch(error => {
+      console.log('Error en cambio ' + error);
     });
   });
   socket.on('conservasa', () => {
