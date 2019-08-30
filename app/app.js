3306e932473897d837a47512805c71897d810cc0
Clemens Hemmerling <clemens@grupomacro.com>
2019-08-30 08:37:41 -0600

    Correcciones

--- app/app.js
+++ app/app.js
@@ -79,13 +79,10 @@ console.log(p12);
 server.listen(port, '0.0.0.0');
 
 io.sockets.on('connect', socket => {
-  const client = {point: row.point, socket: socket};
-  clients[row.secret] = client;
   io.sockets.emit('key', llave);
   socket.on('body', body => {
     xml = body;
     console.log(xml);
-    
     sign();
   });
 
